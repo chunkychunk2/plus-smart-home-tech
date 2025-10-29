311b65e4788df7bc02beff1c3f3421df76bec0fe
chun <chunoff@list.ru>
2025-10-29 23:35:26 +0300

    move config to application.yaml

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -18,13 +19,22 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${analyzer.snapshot.group-id}")
+    private String snapshotGroupId;
+
+    @Value("${analyzer.hub.group-id}")
+    private String hubGroupId;
+
     @Bean
     public ConsumerFactory<String, SensorsSnapshotAvro> snapshotAvroConsumerFactory() {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, SnapshotDeserializer.class);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer.snapshot.group");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, snapshotGroupId);
         return new DefaultKafkaConsumerFactory<>(properties);
     }
 
@@ -38,10 +48,10 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, HubEventAvro> hubEventAvroConsumerFactory() {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, HubEventDeserializer.class);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "analyzer.hub.group");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, hubGroupId);
         return new DefaultKafkaConsumerFactory<>(properties);
     }
 
